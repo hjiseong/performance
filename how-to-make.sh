a5410a16ea7dae5f169373f039d889f307367a0c
hjiseong <84965194+hjiseong@users.noreply.github.com>
2021-08-04 21:05:36 +0900

    [환경설정] 프로젝트 생성 (#1)

---
+++ how-to-make.sh
@@ -0,0 +1,14 @@
+# Initialize project
+npm init -y
+
+# Create frontend folder
+mkdir frontend
+cd frontend
+npm init -y
+
+# Create backend folder
+mkdir backend
+cd ../backend
+npm init -y
+cd ../
+
