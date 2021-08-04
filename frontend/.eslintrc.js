aba7d85938f2549da0c68da4e75fbe035eb6490e
Horong <dgf5820@naver.com>
2021-08-05 02:02:38 +0900

    Fix prettier@typescript-eslint version issue

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,7 +14,9 @@ module.exports = {
   extends: [
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
     "plugin:prettier/recommended"
-  ]
+  ],
+  rules: {
+    "prettier/prettier": 0
+  }
 };
