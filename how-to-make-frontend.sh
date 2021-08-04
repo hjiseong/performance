f3037998c323d1b483af551cf7f5b681f5279c74
Horong <dgf5820@naver.com>
2021-08-05 00:54:53 +0900

    Add craco config file

--- how-to-make-frontend.sh
+++ how-to-make-frontend.sh
@@ -133,3 +133,19 @@ echo 'npx lint-staged && echo "[Husky] pre-commit"' >> .husky/pre-commit
 # Add craco packages
 yarn add @craco/craco
 yarn add -D craco-alias
+
+# Add craco config file
+touch craco.config.js
+echo 'module.exports = {' >> craco.config.js
+echo '  plugins: [' >> craco.config.js
+echo '    {' >> craco.config.js
+echo '      plugin: require("craco-alias"),' >> craco.config.js
+echo '      options: {' >> craco.config.js
+echo '        source: "tsconfig",' >> craco.config.js
+echo '        baseUrl: ".",' >> craco.config.js
+echo '        tsConfigPath: "tsconfig.paths.json",' >> craco.config.js
+echo '        debug: false,' >> craco.config.js
+echo '      },' >> craco.config.js
+echo '    },' >> craco.config.js
+echo '  ],' >> craco.config.js
+echo '};' >> craco.config.js
