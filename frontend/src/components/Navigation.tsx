1eef3802c960e5bb876c5b8d3ffc7d680fa46b55
hjiseong <84965194+hjiseong@users.noreply.github.com>
2021-09-21 00:11:43 +0900

    [기능] 네비게이션 헤더 내용 및 카드 설명 추가 (#31)
    * Apply mix blend mode
    
    * Add navigation web header
    
    * Add mobile or tablet navi header
    
    Co-authored-by: Horong <popomonclub@gmail.com>

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -21,8 +21,10 @@ export default function Navigation({ onClose }: Props): ReactElement {
   function setLeft(l: number) {
     left += l;
     if (wrapper.current && containerRef.current) {
+      const width =
+        window.innerWidth * 0.2 > 450 ? 450 : window.innerWidth * 0.2;
       const start = 0;
-      const end = 500 * 6 - containerRef.current.clientWidth;
+      const end = width * 10 - containerRef.current.clientWidth;
       if (left < start) {
         left = start;
       }
@@ -41,8 +43,10 @@ export default function Navigation({ onClose }: Props): ReactElement {
     window.addEventListener("wheel", function (e) {
       if (wrapper.current && containerRef.current) {
         left += e.deltaY;
+        const width =
+          window.innerWidth * 0.2 > 450 ? 450 : window.innerWidth * 0.2;
         const start = 0;
-        const end = 500 * 6 - containerRef.current.clientWidth;
+        const end = width * 10 - containerRef.current.clientWidth;
         if (left < start) {
           left = start;
         }
@@ -54,15 +58,44 @@ export default function Navigation({ onClose }: Props): ReactElement {
     });
     document.addEventListener("mousemove", (e) => {
       if (cursorRef.current) {
-        cursorRef.current.style.top = `${e.clientY - 140 - 60}px`;
-        cursorRef.current.style.left = `${e.clientX - 60}px`;
+        cursorRef.current.style.top = `${e.clientY - 140 - 65}px`;
+        cursorRef.current.style.left = `${e.clientX - 55}px`;
       }
+    });
+    document.querySelectorAll(".image-content").forEach((link) => {
+      link.addEventListener("mouseover", function (e) {
+        document.querySelector(".cursor")?.classList.add("blend");
+      });
+      link.addEventListener("mouseleave", function (e) {
+        document.querySelector(".cursor")?.classList.remove("blend");
+      });
     });
   }, [wrapper.current, containerRef.current, cursorRef.current]);
 
   return (
     <div className="navigation">
       <header className="nav-header">
+        <div className="nav-info">
+          <span className="nav-dot"></span>
+          <span className="nav-description">
+            <div>(6) INTERNAL WORKS</div>
+            <div>
+              2021 c/o POPO
+              <span className="r-round small">®</span>
+            </div>
+          </span>
+          <span className="nav-description">
+            <div>A COLLECTION OF INTERNAL PROJECT AND INITIATIVES UNDER</div>
+            <div>
+              THE POPO<span className="r-round small">®</span>
+              BRAND.
+            </div>
+          </span>
+        </div>
+        <div className="nav-logo">
+          POPO
+          <span className="r-round small">®</span>
+        </div>
         <button className="nav-close" onClick={() => onClose()}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.7 11.7">
             <path d="M0,10.6L10.6,0l1.1,1.1L1.1,11.7L0,10.6z"></path>
@@ -74,7 +107,9 @@ export default function Navigation({ onClose }: Props): ReactElement {
       <section className="carousel" ref={wrapper}>
         <div className="cursor" ref={cursorRef}>
           <span className="left"></span>
-          <span className="circle">drag</span>
+          <span className="circle">
+            <div className="cursor-text">drag</div>
+          </span>
           <span className="right"></span>
         </div>
         <div className="carousel-container" ref={containerRef}>
@@ -86,6 +121,24 @@ export default function Navigation({ onClose }: Props): ReactElement {
                 alt=""
               />
             </div>
+            <div className="image-content">
+              <h3 className="image-title">
+                <span>
+                  Moves<span className="r-round">®</span>
+                </span>
+                <span>©2019</span>
+              </h3>
+              <div className="image-subtitle">our new hq</div>
+              <div className="image-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nostrum, officia quaerat eius assumenda architecto quisquam
+                facere consequuntur explicabo, odio dolores tempore impedit
+                numquam? Eaque, tempore. Ullam quas iste voluptatum deserunt.
+              </div>
+              <a className="image-link" href="#">
+                Visit the Site
+              </a>
+            </div>
           </div>
           <div className="carousel-card">
             <span className="carousel-card-number">02</span>
@@ -94,6 +147,24 @@ export default function Navigation({ onClose }: Props): ReactElement {
                 src="https://i.pinimg.com/564x/3c/91/3f/3c913f60c02634de039b73462cb4d91e.jpg"
                 alt=""
               />
+            </div>
+            <div className="image-content">
+              <h3 className="image-title">
+                <span>
+                  Moves<span className="r-round">®</span>
+                </span>
+                <span>©2019</span>
+              </h3>
+              <div className="image-subtitle">our new hq</div>
+              <div className="image-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nostrum, officia quaerat eius assumenda architecto quisquam
+                facere consequuntur explicabo, odio dolores tempore impedit
+                numquam? Eaque, tempore. Ullam quas iste voluptatum deserunt.
+              </div>
+              <a className="image-link" href="#">
+                Visit the Site
+              </a>
             </div>
           </div>
           <div className="carousel-card">
@@ -104,6 +175,24 @@ export default function Navigation({ onClose }: Props): ReactElement {
                 alt=""
               />
             </div>
+            <div className="image-content">
+              <h3 className="image-title">
+                <span>
+                  Moves<span className="r-round">®</span>
+                </span>
+                <span>©2019</span>
+              </h3>
+              <div className="image-subtitle">our new hq</div>
+              <div className="image-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nostrum, officia quaerat eius assumenda architecto quisquam
+                facere consequuntur explicabo, odio dolores tempore impedit
+                numquam? Eaque, tempore. Ullam quas iste voluptatum deserunt.
+              </div>
+              <a className="image-link" href="#">
+                Visit the Site
+              </a>
+            </div>
           </div>
           <div className="carousel-card">
             <span className="carousel-card-number">04</span>
@@ -112,6 +201,24 @@ export default function Navigation({ onClose }: Props): ReactElement {
                 src="https://i.pinimg.com/564x/3c/91/3f/3c913f60c02634de039b73462cb4d91e.jpg"
                 alt=""
               />
+            </div>
+            <div className="image-content">
+              <h3 className="image-title">
+                <span>
+                  Moves<span className="r-round">®</span>
+                </span>
+                <span>©2019</span>
+              </h3>
+              <div className="image-subtitle">our new hq</div>
+              <div className="image-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nostrum, officia quaerat eius assumenda architecto quisquam
+                facere consequuntur explicabo, odio dolores tempore impedit
+                numquam? Eaque, tempore. Ullam quas iste voluptatum deserunt.
+              </div>
+              <a className="image-link" href="#">
+                Visit the Site
+              </a>
             </div>
           </div>
           <div className="carousel-card">
@@ -122,6 +229,24 @@ export default function Navigation({ onClose }: Props): ReactElement {
                 alt=""
               />
             </div>
+            <div className="image-content">
+              <h3 className="image-title">
+                <span>
+                  Moves<span className="r-round">®</span>
+                </span>
+                <span>©2019</span>
+              </h3>
+              <div className="image-subtitle">our new hq</div>
+              <div className="image-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nostrum, officia quaerat eius assumenda architecto quisquam
+                facere consequuntur explicabo, odio dolores tempore impedit
+                numquam? Eaque, tempore. Ullam quas iste voluptatum deserunt.
+              </div>
+              <a className="image-link" href="#">
+                Visit the Site
+              </a>
+            </div>
           </div>
           <div className="carousel-card">
             <span className="carousel-card-number">06</span>
@@ -130,6 +255,24 @@ export default function Navigation({ onClose }: Props): ReactElement {
                 src="https://i.pinimg.com/564x/3c/91/3f/3c913f60c02634de039b73462cb4d91e.jpg"
                 alt=""
               />
+            </div>
+            <div className="image-content">
+              <h3 className="image-title">
+                <span>
+                  Moves<span className="r-round">®</span>
+                </span>
+                <span>©2019</span>
+              </h3>
+              <div className="image-subtitle">our new hq</div>
+              <div className="image-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nostrum, officia quaerat eius assumenda architecto quisquam
+                facere consequuntur explicabo, odio dolores tempore impedit
+                numquam? Eaque, tempore. Ullam quas iste voluptatum deserunt.
+              </div>
+              <a className="image-link" href="#">
+                Visit the Site
+              </a>
             </div>
           </div>
         </div>
